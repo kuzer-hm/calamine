8af5a2cd9e7f26c1f54636aaeaf17069fccdc85d
kuzer-hm <iceextinguish@outlook.com>
2024-08-23 14:55:32 +0800

    read picture sheet

--- examples/load_pictures.rs
+++ examples/load_pictures.rs
@@ -3,7 +3,14 @@ use calamine::{open_workbook, Reader, Xlsx};
 
 fn main() {
     // env::set_var("RUST_BACKTRACE","full");
-    let wb: Xlsx<_>  = open_workbook("C:\\Users\\Administrator\\Desktop\\Test\\Test For Xlsx.xlsx").unwrap();
+    // env::set_var("RUST_BACKTRACE", "1");
+    println!("{:?}",env::args().nth(1)) ;
+    // let wb: Xlsx<_>  = open_workbook("../tests/picture.xlsx").unwrap();
+    //
+    // if let Some(drawings) = wb.drawings() {
+    //     println!("ssss");
+    // }
+
     // wb
 }
 
